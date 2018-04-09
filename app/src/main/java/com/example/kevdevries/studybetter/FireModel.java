b00ee910bfea17242b57d503889747e3aca75bf2
Kevin de Vries <kevin.devries@mycit.ie>
2018-04-09 03:43:53 +0100

    Updated CardView to Grinds

---
+++ app/src/main/java/com/example/kevdevries/studybetter/FireModel.java
@@ -0,0 +1,12 @@
+package com.mytrendin.firebaserecyclerviewcardview;
+/**
+ * Created by KevdeVries on 09/04/2018.
+ */
+
+public class FireModel {
+
+    public String title;
+    public String time;
+    public String date;
+    public String recurring;
+}
